f3ac8a3deb3ec6dc0108a8a84b658decf64360c5
Sami <samifiaz@gmail.com>
2020-02-16 14:23:28 +0500

    Update musician.js
    test commit

--- routes/musician.js
+++ routes/musician.js
@@ -9,6 +9,11 @@ router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
+// testing all musicians from data store
+router.get('/sami', (req, res) => {
+    res.status('200').send("Status is OK! baby");
+});
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
